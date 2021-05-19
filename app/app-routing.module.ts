349afd185e1f14bcbcd6bbf188851e168eafcd0c
xmyers27 <macias.angel2727@gmail.com>
2021-05-19 11:28:58 -0700

    first commit for munchies

---
+++ app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: "", pathMatch: "full", redirectTo: "/component"},
+  {path: "contact", component: ContactComponent},
+  {path: "home", component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
